2a8ebb835760379851455210365dcff7a4170071
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-10-29 10:02:58 +0530

    added noteSlice in store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import  userSlice  from './reducers/userSlice'
+import  noteSlice  from './reducers/noteSlice'
 
 export const store = configureStore({
   reducer: {
     user : userSlice,
+    note : noteSlice
   },
 })
