4cae089ad1a883041fb9aa6c3a23dee58c1f1b99
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-10-29 10:05:55 +0530

    added fetch users in app, to make the user persist after login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import React, { useEffect } from 'react'
-import { fetchUser } from './store/actions/userActions'
-import { useDispatch } from 'react-redux'
-import MainRoutes from './Routes/MainRoutes'
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUser } from "./store/actions/userActions";
+import MainRoutes from "./Routes/MainRoutes";
+import NavBar from "./Components/NavBar";
 
 const App = () => {
-  return (
-    <>
-      <MainRoutes/>
-    </>
-  )
-}
+  const dispatch = useDispatch();
 
-export default App
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <NavBar />
+      <MainRoutes />
+    </div>
+  );
+};
+
+export default App;
