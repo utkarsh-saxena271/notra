667ff30fca13235b741e81ed794e4eaddbfbba7f
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-10-29 09:53:11 +0530

    optimised the cookie setter

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -23,8 +23,12 @@ async function registerController(req,res){
         })
 
     const token = jwt.sign({id:user._id}, process.env.JWT_SECRET);
-    res.cookie('token',token)
-
+    res.cookie('token', token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'lax',
+  maxAge: 7 * 24 * 60 * 60 * 1000
+})
     res.status(201).json({
         message: "user registered successfully",
         user:{
@@ -53,7 +57,12 @@ async function loginController(req,res){
          })
     }
     const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
-    res.cookie('token',token)
+    res.cookie('token', token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'lax',
+  maxAge: 7 * 24 * 60 * 60 * 1000
+})
     res.status(201).json({
         message:"user logged in successfully",
         user:{
