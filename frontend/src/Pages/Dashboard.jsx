dcf79c332d4edaa1e7c7e24134423377cc154d40
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-10-29 09:58:08 +0530

    added condional rendering for create note, note list and form

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import CreateNote from '../Components/CreateNote'
+import NoteList from '../Components/NoteList'
+import { Outlet, useLocation } from 'react-router-dom'
 const Dashboard = () => {
+  const location = useLocation()
+  console.log(location)
   return (
-    <div>Dashboard</div>
+    <div>
+      {location.pathname == '/dashboard' && (
+       <> <CreateNote/> <NoteList/></>
+    )}
+      {location.pathname == '/dashboard/createnote' && 
+        <Outlet/>
+      }
+    </div>
   )
 }
 
