629db2069cce60eedecd37680d84bf4ca66cad55
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-10-29 10:03:40 +0530

    optimised user actions

--- frontend/src/store/actions/userActions.js
+++ frontend/src/store/actions/userActions.js
@@ -1,10 +1,10 @@
-import instance from "../../config/AxiosConfig"
+import instance from "../../config/axiosconfig"
 import { loadUser, removeUser } from "../reducers/userSlice";
 
 export const fetchUser = () => async(dispatch, getState) => {
     try {
         const res = await instance.get('/auth/me');
-        dispatch(loadUser(res))
+        dispatch(loadUser(res.data.user))
 
     } catch (error) {
         console.log(error)
@@ -25,6 +25,15 @@ export const signupUser = (data) => async (dispatch, getState) => {
         const res = await instance.post('/auth/register',data)
         dispatch(loadUser(res.data.user))
     }catch(error){
+        console.log(error)
+    }
+}
+
+export const logoutUser = () => async (dispatch, getState) => {
+    try {
+        await instance.post('/auth/logout')
+        dispatch(removeUser())      
+    } catch (error) {
         console.log(error)
     }
 }
