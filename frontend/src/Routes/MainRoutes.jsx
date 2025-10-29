af311a30559ebae4771d896b592ffe0fddcec3ac
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-10-29 10:01:28 +0530

    added lazy loading and also implemented protected and public routes

--- frontend/src/Routes/MainRoutes.jsx
+++ frontend/src/Routes/MainRoutes.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Login from "../Pages/Login";
-import Dashboard from '../Pages/Dashboard';
-import Signup from '../Pages/Signup';
+import ProtectedRoutes from './ProtectedRoutes';
+import PublicRoutes from './PublicRoute';
+import PageNotFound from '../Pages/PageNotFound';
+import CreateNoteForm from '../Pages/CreateNoteForm';
+const Login  = lazy(() => import("../Pages/Login"));
+const Dashboard = lazy(()=>import('../Pages/Dashboard'));
+const Signup = lazy(()=>import('../Pages/Signup'));
 
 const MainRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element = {<Login />} />
-        <Route path='/signup' element = {<Signup />} />
-        <Route path='/dashboard' element = {<Dashboard/>} />
-         
+        <Route path='/' element = {
+          <PublicRoutes>
+            <Login />
+          </PublicRoutes>
+          } />
+        <Route path='/signup' element = {
+          <PublicRoutes>
+            <Signup />
+          </PublicRoutes>
+          } />
+        <Route path='/dashboard' element = {
+          <ProtectedRoutes>
+            <Dashboard/>
+          </ProtectedRoutes>
+          }>
+            <Route path='/dashboard/createnote' element = {
+            <ProtectedRoutes>
+              <CreateNoteForm/>
+            </ProtectedRoutes>
+          }/>
+          </Route>
+          
+        <Route path='*' element={<PageNotFound/>}/>
     </Routes>
   )
 }
