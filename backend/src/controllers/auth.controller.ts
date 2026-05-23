8ac45627b9b304903d0b59f0f3526e2896edbbb3
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-05-23 15:49:01 +0530

    added auth and user controllers

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,50 @@
+import type { Request, Response } from "express";
+import asyncHandler from "../utils/asyncHandler.util.js";
+import { loginService, registerService } from "../services/auth.service.js";
+import ApiResponse from "../utils/apiResponse.util.js";
+import { jwt } from "zod";
+
+export const registerController = asyncHandler(async (req: Request, res: Response) => {
+    const user = await registerService(req.body);
+    res.status(201).json(
+        new ApiResponse(201,
+            {
+                id: user?.id,
+                name: user?.name,
+                email: user?.email
+            },
+            "User registered Successfully"
+        )
+    )
+})
+
+export const loginController = asyncHandler(async (req: Request, res: Response) => {
+    const data = await loginService(req.body);
+    res.cookie('token', data.token)
+    res.status(201).json(
+        new ApiResponse(201,
+            {
+                id: data.user?.id,
+                name: data.user?.name,
+                email: data.user?.email
+
+            },
+            "User registered Successfully"
+        )
+    )
+})
+
+export const logoutController = asyncHandler(
+  async (req: Request, res: Response) => {
+
+    res.clearCookie("token");
+
+    res.status(200).json(
+      new ApiResponse(
+        200,
+        {},
+        "User logged out successfully"
+      )
+    );
+  }
+);
